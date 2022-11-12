e70d98e2cdb413f54b881ef71663aecd7f8ece40
Deven Sitapara <shriramsoft@gmail.com>
2022-11-13 00:21:30 +0530

    signup done

--- frontend/src/hooks/useSignup.js
+++ frontend/src/hooks/useSignup.js
@@ -1,36 +1,71 @@
 import { useState } from 'react';
 import { useAuthContext } from './useAuthContext';
 import config from './../config';
+import { gql, useMutation } from '@apollo/client';
+
+export const registrationMutationGQL  = gql`
+mutation($registerInput: RegisterInput){
+    register(registerInput: $registerInput){
+      user {
+        email
+        id
+        name
+        name
+      }
+      tokens {
+        access {
+          expires
+          token
+        }
+      }
+    }
+  }`;
+
 
 export const useSignup = () => {
     const [error, setError] = useState(null);
     const [isLoading, setIsLoading] = useState(null);
     const { dispatch } = useAuthContext();
+    const [mutateRegistration] = useMutation(registrationMutationGQL);
 
     const signup = async (email, password) => {
         setIsLoading(true);
         setError(null);
 
-        const response = await fetch(config.apiBasePath + '/auth/register', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ name: 'fake name', email, password })
-        });
-        const json = await response.json();
+        // const response = await fetch(config.apiBasePath + '/auth/register', {
+        //     method: 'POST',
+        //     headers: { 'Content-Type': 'application/json' },
+        //     body: JSON.stringify({ name: 'fake name', email, password })
+        // });
+        // const json = await response.json();
         // console.log('json');
         // console.log(json);
-        if (!response.ok) {
+
+        const response = await mutateRegistration({
+            variables:{
+                "registerInput": {
+                    "name": "Fake User", 
+                    "password": password,
+                    "email": email
+                    }
+                },
+          });
+         console.log('response.data.register');
+         console.log(response.data.register);
+        
+
+         if (response.errors && response.errors.message) {
             setIsLoading(false);
-            setError(json.error);
+            setError(response.errors[0].message);
             // update the auth context
-            dispatch({ type: 'REGISTER_ERROR', payload: json });
+            dispatch({ type: 'REGISTER_ERROR', payload: response });
         }
-        if (response.ok) {
+        if (response?.data?.register) {
             // save the user to local storage
-            localStorage.setItem('user', JSON.stringify(json));
+            localStorage.setItem('user', JSON.stringify(response.data.register));
 
             // update the auth context
-            dispatch({ type: 'LOGIN', payload: json });
+            dispatch({ type: 'LOGIN', payload: response.data.register });
 
             // update loading state
             setIsLoading(false);
